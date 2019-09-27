f029320983ddabe44124bcca9090154c6bc8e067
soma kesalu <skesalu@sapient.com>
2019-09-27 13:07:29 +0530

    deep neural network with 5 layers

---
+++ iris_application_vectorization.py
@@ -0,0 +1,134 @@
+from load_data import load_dataset
+import pdb
+import numpy as np
+
+
+train_data_x, test_data_x, train_output_y, test_output_y = load_dataset()
+test_output_y[test_output_y == 'Iris-setosa'] = 1
+test_output_y[test_output_y != 1] = 0
+train_output_y[train_output_y == 'Iris-setosa'] = 1
+train_output_y[train_output_y != 1] = 0
+print("Number of training examples: m_train = " + str(train_data_x.shape[0]))
+print("Number of testing examples: m_test = " + str(test_data_x.shape[0]))
+print("train_set_x shape: " + str(train_data_x.shape))
+print("train_set_y shape: " + str(train_output_y.shape))
+print("test_set_x shape: " + str(test_data_x.shape))
+print("test_set_y shape: " + str(test_output_y.shape))
+
+train_data_x = train_data_x.astype(float).reshape(train_data_x.shape[1],train_data_x.shape[0])
+test_data_x = test_data_x.astype(float).reshape(test_data_x.shape[1],test_data_x.shape[0])
+train_output_y = train_output_y.astype(float).reshape(train_output_y.shape[1],train_output_y.shape[0])
+test_output_y = test_output_y.astype(float).reshape(test_output_y.shape[1],test_output_y.shape[0])
+
+#initialize parameters
+
+
+layer_dims = [4,5,4,3,2,1]
+
+def initialize_parameters(layer_dims):
+ parameters = {}
+ for i in range(1,len(layer_dims)):
+
+  parameters["w" + str(i)] = (np.random.randn(layer_dims[i],layer_dims[i-1]))*(np.sqrt(2/(layer_dims[i])))
+  parameters["b" + str(i)] = np.zeros(shape=(layer_dims[i],1))
+
+ return parameters
+
+
+def forward_propagation_relu(w,b,a_prev):
+ z = np.dot(w,a_prev)+b
+ a = np.maximum(0, z)
+ linear_cache = w,a_prev,b,z
+ activation_cache = z
+ cache = (linear_cache,activation_cache)
+ return a,cache
+
+def forward_propagation_sigmoid(w,b,a_prev):
+ z = np.dot(w,a_prev)+b
+ a = 1 / (1 + np.exp(-z))
+ linear_cache = w,a_prev,b,z
+ activation_cache = z
+ cache = (linear_cache,activation_cache)
+ return a,cache
+
+def back_propagation_sigmoid(da_prev,cache):
+ linear_cache,activation_cache = cache
+ w,a_prev,b,z = linear_cache
+ s = 1 / (1 + np.exp(-activation_cache))
+ dZ = da_prev * s * (1 - s)
+ dw = np.dot(dZ, a_prev.T)
+ db = np.sum(dZ, axis=1, keepdims=True)
+ da = np.dot(w.T, dZ)
+
+ return dw,db,da
+
+def back_propagation_relu(da_prev,cache):
+
+ linear_cache,activation_cache = cache
+ w,a_prev,b,z = linear_cache
+ dZ = np.array(da_prev, copy=True)
+ dZ[z <= 0] = 0
+ dw = np.dot(dZ, a_prev.T)
+ db = np.sum(dZ, axis=1, keepdims=True)
+ da = np.dot(w.T, dZ)
+
+ return dw,db,da
+
+def update_parameters(parameters,grads,learning_rate):
+ for i in range(0,5):
+  parameters["w" + str(i+1)] = parameters["w" + str(i+1)] - learning_rate*grads['dw'+str(i+1)]
+  parameters["b" + str(i+1)] = parameters["b" + str(i+1)] - learning_rate*grads['db'+str(i+1)]
+
+ return parameters
+
+def predict(x,parameters):
+ m = x.shape[1]
+ p = np.zeros((1, m))
+ probas,cache = model_forward(x,parameters)
+ pdb.set_trace()
+ for i in range(0, probas.shape[1]):
+  if probas[0, i] > 0.8:
+    p[0, i] = 1
+  else:
+    p[0, i] = 0
+
+ return p
+
+
+
+def model_forward(train_data,parameters):
+ caches = []
+ a_prev = train_data
+ for i in range(1,5):
+  w,b = parameters['w'+str(i)],parameters['b'+str(i)]
+  a_prev,cache = forward_propagation_relu(w,b,a_prev)
+  caches.append(cache)
+ a_prev,cache = forward_propagation_sigmoid(parameters['w5'],parameters['b5'],a_prev)
+ caches.append(cache)
+ return a_prev,caches
+
+def model_backward(al,y,caches):
+ grads = {}
+ dal = - (np.divide(y, al) - np.divide(1 - y, 1 - al))
+ grads['dw5'],grads['db5'],grads['da4'] = back_propagation_sigmoid(dal,caches[4])
+ for l in reversed(range(4)):
+  grads['dw'+str(l+1)], grads['db'+str(l+1)], grads['da'+str(l)] = back_propagation_relu(grads['da'+str(l+1)],caches[l])
+
+ return grads
+
+def model(train_data_x,train_output_y,learning_rate,num_iterations,layer_dims):
+ a_prev = train_data_x
+ y = train_output_y
+ parameters = initialize_parameters(layer_dims)
+ for i in range(0, num_iterations):
+  al,caches = model_forward(a_prev,parameters)
+  grads = model_backward(al,y, caches)
+  update_parameters(parameters,grads,learning_rate)
+
+ return parameters
+
+
+parameters = model(train_data_x,train_output_y,0.0001,20000,layer_dims)
+pred_test = predict(test_data_x, parameters)
+print(pred_test)
+pdb.set_trace()
